a46a305507665b96c56b03849586b95a88cbeb65
Archugov <archugov.pavel@gmail.com>
2021-12-06 17:26:38 +0300

    Добавьте файлы проекта.

---
+++ Exersizes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exersizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
